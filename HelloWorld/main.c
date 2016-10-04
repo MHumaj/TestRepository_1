1c8b948dcafac32c562f19d196bea3fe5bb479c4
MHumaj <mhumaj@danfoss.com>
2016-10-04 11:09:16 +0200

    Changed var_a
    Changed var_a

---
+++ HelloWorld/main.c
@@ -0,0 +1,17 @@
+/* Hello World */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int var_a;
+unsigned int var_b;
+
+int main()
+{
+    var_a = 30U;
+    var_b = 20U;
+
+    printf("Hello World\n\n%d + %d = %d",var_a,var_b,var_a+var_b);
+
+
+}
